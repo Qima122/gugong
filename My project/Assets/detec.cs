6cb96e85914ad17fe0af03dbd62dea5bf52d5a57
Bingshuang0u0 <bingshuang0w0@gmail.com>
2024-01-28 12:25:03 +0800

    Update detec.cs
    Player can now interact with button
    Look at the button + Press E can make button appear and disappear while player is inside the detection area

--- My project/Assets/detec.cs
+++ My project/Assets/detec.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class detec : MonoBehaviour
 {
     public GameObject UI1;
+    bool entered = false;
+    bool triggered = false;
     // Start is called before the first frame update
     private void OnTriggerEnter(UnityEngine.Collider other)
     {
         if (other.CompareTag("Cb1"))
         {
             UI1.SetActive(true);
+            entered = true;
         }
         
     }
@@ -20,6 +23,7 @@ public class detec : MonoBehaviour
         if (other.CompareTag("Cb1"))
         {
             UI1.SetActive(false);
+            entered = false;
         }
 
     }
@@ -30,6 +34,20 @@ public class detec : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (entered)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (!triggered) {
+                    UI1.SetActive(false);
+                    triggered = true;
+                }
+                else
+                {
+                    UI1.SetActive(true);
+                    triggered = false;
+                }
+            }
+        }
     }
 }
